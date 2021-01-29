435d8eb6a6b065d58bd2ed814baeb6f03efc34a9
Sam Fisher <dragonlss@hotmail.com>
2021-01-28 18:50:53 -0600

    Added placeholder scripts. Added player prefab variant.

---
+++ Assets/Scripts/NPC/NPCBase.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tenet.NPC
+{
+    public class NPCBase : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
