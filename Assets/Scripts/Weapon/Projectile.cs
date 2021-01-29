640e691868e4b32cf5c0c88ff8c4b3f1c372ca26
Sam Fisher <dragonlss@hotmail.com>
2021-01-29 11:56:21 -0600

    Fixed projectile issues. Flag to not destroy on damage, not destroy after lifetime, kinematic on damage. - Updated projectile prefabs.

--- Assets/Scripts/Weapon/Projectile.cs
+++ Assets/Scripts/Weapon/Projectile.cs
@@ -11,7 +11,10 @@ namespace Tenet.Weapon
         [SerializeField] private float Speed = 5.0f; // m/s
 		[SerializeField] private float MaxLifetime = 2.0f; // seconds
 
+		[SerializeField] private bool DestroyAfterLifetime = false;
+		[SerializeField] private bool DestroyOnDamage = false;
 		[SerializeField] private bool DamageOnCollision = true;
+		[SerializeField] private bool KinematicOnDamage = false;
 		[SerializeField] private bool RandomSpin = false;
 		[SerializeField] private Rigidbody Rigidbody;
 
@@ -26,22 +29,14 @@ namespace Tenet.Weapon
 				transform.Translate(0, 0, Speed * Time.deltaTime, Space.Self);
 		}
 
-		private void OnDestroy()
-		{
-			if (!DamageOnCollision)
-			{
-				Debug.Log($"Projectile {SourceAmmo.name} damage after duration.");
-				SourceAmmo.ApplyDamage(null, transform.position);
-
-				if (DamageEffect != null)
-					Instantiate(DamageEffect, transform.position, transform.rotation);
-			}
-		}
-
 		public void Configure(Ammo SourceAmmo)
         {
             this.SourceAmmo = SourceAmmo;
-			Destroy(gameObject, MaxLifetime);
+			if (DestroyAfterLifetime)
+				Destroy(gameObject, MaxLifetime);
+
+			if (!DamageOnCollision)
+				Invoke(nameof(ApplyDamage), MaxLifetime);
 
 			if (Rigidbody != null)
 			{
@@ -60,17 +55,30 @@ namespace Tenet.Weapon
 			}
 		}
 
-		private void OnCollisionEnter(Collision collision)
-		{
-			if (!DamageOnCollision)
-				return;
+		private void ApplyDamage() => ApplyDamage(null, transform.position, $"damage after duration {MaxLifetime}s");
 
-            Debug.Log($"Projectile {SourceAmmo.name} hit {collision.gameObject}.", collision.gameObject);
-			SourceAmmo.ApplyDamage(collision.gameObject, collision.GetContact(0).point);
-			Destroy(gameObject);
+		private void ApplyDamage(GameObject Target, Vector3 Location, string DebugMessage)
+		{
+			Debug.Log($"Projectile {SourceAmmo.name} {DebugMessage}.");
+			SourceAmmo.ApplyDamage(Target, Location);
+
+			if (KinematicOnDamage && Rigidbody != null)
+			{
+				Rigidbody.isKinematic = true;
+				Rigidbody.detectCollisions = false;
+			}
+			
+			if (DestroyOnDamage)
+				Destroy(gameObject);
 
 			if (DamageEffect != null)
 				Instantiate(DamageEffect, transform.position, transform.rotation);
+		}
+
+		private void OnCollisionEnter(Collision collision)
+		{
+			if (DamageOnCollision)
+				ApplyDamage(collision.gameObject, collision.GetContact(0).point, $"hit {collision.gameObject}");
 		}
 	}
 }
